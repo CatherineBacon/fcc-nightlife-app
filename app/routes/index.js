1135aec7ea03e66fa4fd5a4bcd1ab05948b4cc85
catherine <catherine.bacon@gmail.com>
2017-02-20 16:43:55 +0000

    add jade

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,20 @@
 'use strict'
 
-var path = process.cwd();
+//var path = process.cwd();
+var path = require('path');
 
 module.exports = function (app) {
+	app.set('views', path.join(__dirname, '../views'));
+	app.set('view engine', 'jade');
 
-	app.route('/')
-		.get(function (req, res) {
-			res.sendFile(path + '/app/views/index.html' )
-		});
+	app.get('/', function(req, res) {
+		res.render('index');
+	});
+
+	//app.route('/')
+	//	.get(function (req, res) {
+	//		res.sendFile(path + '/app/views/index.html' )
+	//	});
 
 	app.route('/api/:loc')
 		.get(function (req, res) {
--- server.js
+++ server.js
@@ -2,12 +2,15 @@
 
 var express = require('express');
 var routes = require('./app/routes/index.js');
-
 var app = express();
+
+var path = require('path');
+var port = process.env.PORT || 8080;
+
 
 routes(app);
 
-var port = process.env.PORT || 8080;
+
 app.listen(port, function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
