591d087d96003199c893032bcfc4f4311ef621e2
catherine <catherine.bacon@gmail.com>
2017-02-20 16:48:24 +0000

    move todos to readme

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # fcc-nightlige-app
+
+
+## TO DO 
+- [x] display something on localhost:8080
+- [x] box where can type location
+- [x] jade
+- [ ] box submits and checks validity
+- [ ] display fake data bars
+- [ ] mongodb set up
+- [ ] voting - only once
+- [ ] attendance tonight - times in mongo
+- [ ] yelp api
+- [ ] add auth
+- [ ] heroku
--- server.js
+++ server.js
@@ -15,13 +15,3 @@ app.listen(port, function () {
 	console.log('Node.js listening on port ' + port + '...');
 });
 
-//// TO DO ////
-// + display something on localhost:8080
-// - box where can type location
-// - display fake data bars
-// - mongodb set up
-// - voting - only once
-// - attendance tonight - times in mongo
-// - yelp api
-// - add auth
-// - heroku
