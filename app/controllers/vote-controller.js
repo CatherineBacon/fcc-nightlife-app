f6d1e18d0a9748e24b7853e605ad8a0fc6674d09
catherine <catherine.bacon@gmail.com>
2017-02-22 13:21:36 +0000

    update logic in vote model so only vote once

--- app/controllers/vote-controller.js
+++ app/controllers/vote-controller.js
@@ -5,11 +5,22 @@ var Vote = require('../models/vote.js');
 module.exports = {
 	vote: function(req, res) {
 		var barId = req.params.barId,
-			place = req.params.place;
-		var vote = new Vote({ bar: barId, user: 'x', date: new Date() });
-		vote.save(function(err, vote) {
+			place = req.params.place,
+			sixteenHours = 16 * 60 * 60 * 1000, /*16 hours in ms*/
+			sixteenHoursAgo = new Date(new Date() - sixteenHours);
+		Vote.find( { 'bar': barId, 'date': {$gt: sixteenHoursAgo} }, function(err, votes) {
 			if (err) console.log(err);
-			res.redirect(`/bars/${place}`)
+			var voted = votes.filter(function(v) {
+							return v.user=='x' && v.bar==bar.id; /* WILL NEED UPDATING WHEN ADD USER AUTH */
+						}).length > 0 ;
+			if (voted) res.redirect(`/bars/${place}`);
+			else {
+				var vote = new Vote({ bar: barId, user: 'x', date: new Date() });
+				vote.save(function(err, vote) {
+					if (err) console.log(err);
+					res.redirect(`/bars/${place}`)
+				});
+			}
 		});
 	}
 }
