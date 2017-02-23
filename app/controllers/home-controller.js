4dac77d07c51dc6c71041743ce3dc97032c3c636
catherine <catherine.bacon@gmail.com>
2017-02-23 11:13:10 +0000

    access userid and username

--- app/controllers/home-controller.js
+++ app/controllers/home-controller.js
@@ -3,11 +3,20 @@
 module.exports = {
 	home: function(req, res) {
 		var isLoggedIn = req.isAuthenticated();
-		res.render('index', {isLoggedIn: isLoggedIn});
+		var user = req.user;
+		res.render('index', {
+			isLoggedIn: isLoggedIn,
+			user: user,
+		});
 	},
 	search: function(req, res) {
 		var location = req.body.location;
 		var isLoggedIn = req.isAuthenticated();
-		res.redirect(`/bars/${location}`, {isLoggedIn: isLoggedIn});
+		var user = req.user;
+		res.redirect(`/bars/${location}`, {
+			isLoggedIn: isLoggedIn,
+			username: user.username,
+			user: user,
+		});
 	},
 }
